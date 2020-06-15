978f4a967275b4b20b1dcdf1ab19c54386733e42
Master Kenth <masterkenth@gmail.com>
2020-06-15 19:51:26 +0200

    style: eslint compliance.

--- webserver/index.js
+++ webserver/index.js
@@ -4,10 +4,10 @@ const app = express();
 app.set('json spaces', 2);
 
 app.use('/', (req, res) => { res.json({
-        protocol: req.protocol,
-        baseUrl: req.baseUrl,
-        subdomains: req.subdomains,
-        hostname: req.hostname
+    protocol: req.protocol,
+    baseUrl: req.baseUrl,
+    subdomains: req.subdomains,
+    hostname: req.hostname
 }); });
 
-app.listen(80, () => { console.log("Server up"); });
+app.listen(80, () => { console.log('Server up'); });
