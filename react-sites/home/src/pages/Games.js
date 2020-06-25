6d1c221bf4ccf8ed2f22d7bced895be223d0e9c0
Master Kenth <masterkenth@gmail.com>
2020-06-25 16:41:21 +0200

    feat: (home) base Games page layout.

--- react-sites/home/src/pages/Games.js
+++ react-sites/home/src/pages/Games.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
+import Styles from './Games.module.scss'
 
 export default function Games () {
   const { data, loading, error } = useQuery(gql`
@@ -16,23 +17,40 @@ export default function Games () {
     }
   `)
 
-  return <div>
-    <h1>Games</h1>
-    {
-      (!!error || !data) && !loading ? (
-        error
-          ? (<span>{error.message}</span>)
-          : (<span>Unknown error fetching data</span>)
-      ) : (
-        loading ? (<span>Loading...</span>)
-          : (data.games.map((game, index) => (
-            <div key={`game_${index}`}>
-              <NavLink to={`/games/${game._id}`}>{game.title}</NavLink> - <span>{game.desc}</span>
-            </div>
-          )))
-      )
-    }
-  </div>
+  return (
+    <div className={Styles.root}>
+      <div className={Styles.rootCard}>
+        <div>
+          <NavLink to={'/'}>⮜ Home</NavLink>
+        </div>
+        <div className={Styles.header}>
+          <h1>Games</h1>
+          <span>A list of game projects I&apos;m working on or have worked on in the past</span>
+        </div>
+        <div className={Styles.listContainer}>
+          {
+            (!!error || !data) && !loading ? (
+              error
+                ? (<span>{error.message}</span>)
+                : (<span>Unknown error fetching data</span>)
+            ) : (
+              loading ? (<span>Loading...</span>)
+                : (data.games.map((game, index) => (
+                  <NavLink key={`game_${index}`} to={`/games/${game._id}`}>
+                    <div className={Styles.listCard} style={{ backgroundImage: 'url(http://api.masterkenth-test.com/_files/main/games/sky_climb_thumb.png)' }}>
+                      <div className={Styles.content}>
+                        <h2>{game.title}</h2>
+                        <p>{game.desc}</p>
+                      </div>
+                    </div>
+                  </NavLink>
+                )))
+            )
+          }
+        </div>
+      </div>
+    </div>
+  )
 }
 
 Games.propTypes = {
