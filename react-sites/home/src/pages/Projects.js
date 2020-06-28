67be493320c071a953d45f41ef32d866f79822de
Master Kenth <masterkenth@gmail.com>
2020-06-28 02:24:25 +0200

    feat: (home) base Projects page layout.

--- react-sites/home/src/pages/Projects.js
+++ react-sites/home/src/pages/Projects.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
+import Styles from './Projects.module.scss'
+import SharedStyles from '../Shared.module.scss'
 
 export default function Projects () {
   const { data, loading, error } = useQuery(gql`
@@ -14,23 +16,41 @@ export default function Projects () {
     }
   `)
 
-  return <div>
-    <h1>Projects</h1>
-    {
-      (!!error || !data) && !loading ? (
-        error
-          ? (<span>{error.message}</span>)
-          : (<span>Unknown error fetching data</span>)
-      ) : (
-        loading ? (<span>Loading...</span>)
-          : (data.projects.map((project, index) => (
-            <div key={`project_${index}`}>
-              <a href={`https://${project.subdomain}.${window.location.hostname}/`}>{project.title}</a> - <span>{project.desc}</span>
-            </div>
-          )))
-      )
-    }
-  </div>
+  return (
+    <div className={SharedStyles.pageRoot}>
+      <div className={SharedStyles.rootCard}>
+        <div>
+          <NavLink to={'/'}>⮜ Home</NavLink>
+        </div>
+        <div className={SharedStyles.header}>
+          <h1>Projects</h1>
+          <span>A list of various non-game projects (websites, apps etc.) I&apos;ve made or I&apos;m currently working on</span>
+        </div>
+        <div className={Styles.listContainer}>
+          {
+            (!!error || !data) && !loading ? (
+              error
+                ? (<span>{error.message}</span>)
+                : (<span>Unknown error fetching data</span>)
+            ) : (
+              loading ? (<span>Loading...</span>)
+                : (data.projects.map((project, index) => (
+                  <a key={`project_${index}`} href={`https://${project.subdomain}.${window.location.hostname}/`}>
+                    <div className={Styles.listCard}>
+                      <img src={'http://api.masterkenth-test.com/_files/main/games/sky_climb_thumb.png'} />
+                      <div className={Styles.content}>
+                        <h2>{project.title}</h2>
+                        <p>{project.desc}</p>
+                      </div>
+                    </div>
+                  </a>
+                )))
+            )
+          }
+        </div>
+      </div>
+    </div>
+  )
 }
 
 Projects.propTypes = {
