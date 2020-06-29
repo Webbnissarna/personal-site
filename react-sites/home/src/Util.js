f6a9b005d4e835fd663f5d41659da6151e8c8828
Master Kenth <masterkenth@gmail.com>
2020-06-30 01:04:50 +0200

    feat: (home) add utilities js file.

---
+++ react-sites/home/src/Util.js
@@ -0,0 +1,13 @@
+exports.formatDateNumber = function (num) {
+  const date = new Date(Number.parseInt(num))
+  const yyyy = date.getUTCFullYear().toString()
+  const mm = (date.getUTCMonth() + 1).toString().padStart(2, '0')
+  const dd = date.getUTCDate().toString().padStart(2, '0')
+  const H = date.getUTCHours().toString().padStart(2, '0')
+  const M = date.getUTCMinutes().toString().padStart(2, '0')
+  return `${yyyy}-${mm}-${dd} ${H}:${M} (UTC)`
+}
+
+exports.getStaticContentUrl = function (key) {
+  return `${window.location.protocol}//api.${window.location.hostname}/_files/main/${key}`
+}
