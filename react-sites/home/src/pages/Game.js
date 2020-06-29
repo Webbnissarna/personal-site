04a92d04670169a3fb3f89a07aa3218ff8c59b7c
Master Kenth <masterkenth@gmail.com>
2020-06-30 01:38:41 +0200

    feat: (home) base Game page layout.

--- react-sites/home/src/pages/Game.js
+++ react-sites/home/src/pages/Game.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
+import SharedStyles from '../Shared.module.scss'
+import ReactMarkdown from 'react-markdown'
+import Util from '../Util.js'
 
 export default function Game () {
   const { id } = useParams()
@@ -15,17 +18,46 @@ export default function Game () {
     }
   `)
 
-  return <div>
-    { !!error && <span>Error: {`${error}`}</span> }
-    { loading ? (<span>Loading...</span>) : (
-      <div>
-        <b>Title: </b><span>{data.game.title}</span><br />
-        <b>Release Date: </b><span>{new Date(Number.parseInt(data.game.release_date)).toISOString()}</span><br />
-        <b>Body: </b><span>{data.game.body}</span>
+  return (
+    <div className={SharedStyles.pageRoot}>
+      <div className={SharedStyles.rootCard}>
+        <div>
+          <NavLink to={'/games'}>⮜ Games</NavLink>
+        </div>
+        {
+          (!!error || !data) && !loading ? (
+            error
+              ? (<span>{error.message}</span>)
+              : (<span>Unknown error fetching data</span>)
+          ) : (
+            loading ? (<span>Loading...</span>)
+              : (<>
+                <div className={SharedStyles.header}>
+                  <img className={SharedStyles.headerImage} alt="" src={'http://api.masterkenth-test.com/_files/main/games/sky_climb_thumb.png'} />
+                  <h1>{data.game.title}</h1>
+                  <span className={SharedStyles.subtitle}>{Util.formatDateNumber(data.game.release_date)}</span>
+                </div>
+                <div>
+                  <ReactMarkdown source={data.game.body} />
+                </div>
+              </>)
+          )
+        }
       </div>
-    )
-    }
-  </div>
+    </div>
+  )
+
+  // <div>
+  //  { !!error && <span>Error: {`${error}`}</span> }
+  //  { loading ? (<span>Loading...</span>) : (
+  //    <div>
+  //      <b>Title: </b><span>{data.game.title}</span><br />
+  //      <b>Release Date: </b><span>{new Date(Number.parseInt(data.game.release_date)).toISOString()}</span><br />
+  //      <b>Body: </b><span>{data.game.body}</span>
+  //    </div>
+  //  )
+  //  }
+  // </div>
 }
 
 Game.propTypes = {
