9744eb35c44b1ebdae2bc772b18a6983e940477c
Master Kenth <masterkenth@gmail.com>
2020-06-30 01:11:55 +0200

    feat: (home) base Notes/Note page layout.

--- react-sites/home/src/pages/Note.js
+++ react-sites/home/src/pages/Note.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
 import ReactMarkdown from 'react-markdown'
+import Styles from './Note.module.scss'
+import SharedStyles from '../Shared.module.scss'
+import Util from '../Util.js'
 
 export default function Note () {
   const { id } = useParams()
@@ -10,24 +13,53 @@ export default function Note () {
     query getNote {
       note(_id: "${id}") {
         title
+        img_key
         post_date
         body
       }
     }
   `)
 
-  return <div>
-    { !!error && <span>Error: {`${error}`}</span> }
-    { loading ? (<span>Loading...</span>) : (
-      <div>
-        <b>Title: </b><span>{data.note.title}</span><br />
-        <b>Post Date: </b><span>{new Date(Number.parseInt(data.note.post_date)).toISOString()}</span><br />
-        <b>Body: </b><br />
-        <ReactMarkdown source={data.note.body} />
+  return (
+    <div className={SharedStyles.pageRoot}>
+      <div className={SharedStyles.rootCard}>
+        <div>
+          <NavLink to={'/notes'}>⮜ Notes</NavLink>
+        </div>
+        {
+          (!!error || !data) && !loading ? (
+            error
+              ? (<span>{error.message}</span>)
+              : (<span>Unknown error fetching data</span>)
+          ) : (
+            loading ? (<span>Loading...</span>)
+              : (<>
+                <div className={SharedStyles.header}>
+                  { data.note.img_key && <img className={Styles.headerImage} alt="" src={Util.getStaticContentUrl(data.note.img_key)} /> }
+                  <h1>{data.note.title}</h1>
+                  <span className={Styles.subtitle}>{Util.formatDateNumber(data.note.post_date)}</span>
+                </div>
+                <div>
+                  <ReactMarkdown source={data.note.body} />
+                </div>
+              </>)
+          )
+        }
       </div>
-    )
-    }
-  </div>
+    </div>
+  )
+  // <div>
+  //  { !!error && <span>Error: {`${error}`}</span> }
+  //  { loading ? (<span>Loading...</span>) : (
+  //    <div>
+  //      <b>Title: </b><span>{data.note.title}</span><br />
+  //      <b>Post Date: </b><span>{new Date(Number.parseInt(data.note.post_date)).toISOString()}</span><br />
+  //      <b>Body: </b><br />
+  //      <ReactMarkdown source={data.note.body} />
+  //    </div>
+  //  )
+  //  }
+  // </div>
 }
 
 Note.propTypes = {
--- react-sites/home/src/pages/Notes.js
+++ react-sites/home/src/pages/Notes.js
@@ -3,37 +3,81 @@ import { NavLink } from 'react-router-dom'
 import gql from 'graphql-tag'
 import { useQuery } from '@apollo/react-hooks'
 import ReactMarkdown from 'react-markdown'
+import Styles from './Notes.module.scss'
+import SharedStyles from '../Shared.module.scss'
+import Util from '../Util.js'
 
 export default function Notes () {
   const { data, loading, error } = useQuery(gql`
-    query getGames {
+    query getNotes {
       notes {
         _id
         title
+        img_key
         desc
         post_date
       }
     }
   `)
 
-  return <div>
-    <h1>Notes</h1>
-    {
-      (!!error || !data) && !loading ? (
-        error
-          ? (<span>{error.message}</span>)
-          : (<span>Unknown error fetching data</span>)
-      ) : (
-        loading ? (<span>Loading...</span>)
-          : (data.notes.map((note, index) => (
-            <div key={`note_${index}`}>
-              <NavLink to={`/notes/${note._id}`}>{note.title}</NavLink> -
-              <ReactMarkdown source={note.desc} disallowedTypes={['paragraph']} unwrapDisallowed />
-            </div>
-          )))
-      )
-    }
-  </div>
+  return (
+    <div className={SharedStyles.pageRoot}>
+      <div className={SharedStyles.rootCard}>
+        <div>
+          <NavLink to={'/'}>⮜ Home</NavLink>
+        </div>
+        <div className={SharedStyles.header}>
+          <h1>Notes</h1>
+          <span>A collection of notes of various topics</span>
+        </div>
+        <div className={Styles.listContainer}>
+          {
+            (!!error || !data) && !loading ? (
+              error
+                ? (<span>{error.message}</span>)
+                : (<span>Unknown error fetching data</span>)
+            ) : (
+              loading ? (<span>Loading...</span>)
+                : (data.notes.map((note, index) => (
+                  <NavLink key={`note_${index}`} to={`/notes/${note._id}`}>
+                    <div className={Styles.listCard}>
+                      <div className={Styles.content}>
+                        { note.img_key && <img alt="" src={Util.getStaticContentUrl(note.img_key)} /> }
+                        <div className={Styles.innerContent}>
+                          <h2>{note.title}</h2>
+                          <div>
+                            <ReactMarkdown source={note.desc} disallowedTypes={['paragraph']} unwrapDisallowed />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </NavLink>
+                )))
+            )
+          }
+        </div>
+      </div>
+    </div>
+  )
+
+  // <div>
+  //  <h1>Notes</h1>
+  //  {
+  //    (!!error || !data) && !loading ? (
+  //      error
+  //        ? (<span>{error.message}</span>)
+  //        : (<span>Unknown error fetching data</span>)
+  //    ) : (
+  //      loading ? (<span>Loading...</span>)
+  //        : (data.notes.map((note, index) => (
+  //          <div key={`note_${index}`}>
+  //            <NavLink to={`/notes/${note._id}`}>{note.title}</NavLink> -
+  //            <ReactMarkdown source={note.desc} disallowedTypes={['paragraph']} unwrapDisallowed />
+  //          </div>
+  //        )))
+  //    )
+  //  }
+  // </div>
 }
 
 Notes.propTypes = {
