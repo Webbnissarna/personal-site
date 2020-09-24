da7185a54adc07dc85215afc11ecc8301de9b057
Master Kenth <masterkenth@gmail.com>
2020-09-24 17:34:06 +0200

    feat: add About page content

--- react-sites/home/src/pages/About.js
+++ react-sites/home/src/pages/About.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import SharedStyles from '../Shared.module.scss'
+import Note from './Note'
 
 export default function About () {
   return (
-    <div className={SharedStyles.pageRoot}>
-      <div className={SharedStyles.rootCard}>
-        <div>
-          <NavLink to={'/'}>⮜ Home</NavLink>
-        </div>
-        <div className={SharedStyles.header}>
-          <h1>About Me</h1>
-        </div>
-      </div>
-    </div>
+    <Note explicitID='about' />
   )
 }
 
