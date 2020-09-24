1293970beca36110b0553d6a0df5a8962160ac0c
Master Kenth <masterkenth@gmail.com>
2020-09-24 14:51:42 +0200

    feat: add CV page content

--- react-sites/home/src/pages/CV.js
+++ react-sites/home/src/pages/CV.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import SharedStyles from '../Shared.module.scss'
+import Note from './Note'
 
 export default function CV () {
   return (
-    <div className={SharedStyles.pageRoot}>
-      <div className={SharedStyles.rootCard}>
-        <div>
-          <NavLink to={'/'}>⮜ Home</NavLink>
-        </div>
-        <div className={SharedStyles.header}>
-          <h1>CV</h1>
-        </div>
-      </div>
-    </div>
+    <Note explicitID='cv' />
   )
 }
 
